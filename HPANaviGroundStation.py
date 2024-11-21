d592033e03ea83113f6e2e2a576b514f5e3ef366
HirakuTOIDA <hirakutoida@gmail.com>
2024-11-21 23:17:18 +0900

    Page L, N, O, P, and R are added.

--- HPANaviGroundStation.py
+++ HPANaviGroundStation.py
@@ -61,15 +61,20 @@ pagea = SylphideProcessor.PageA()
 pagef = SylphideProcessor.PageF()
 pageg = SylphideProcessor.PageG()
 pageh = SylphideProcessor.PageH()
+pagel = SylphideProcessor.PageL()  # todo
 pagem = SylphideProcessor.PageM()
 pagen = SylphideProcessor.PageN()
+pageo = SylphideProcessor.PageO()  # todo
+pagep = SylphideProcessor.PageP()
+pager = SylphideProcessor.PageR()
 pages = SylphideProcessor.PageS()
+paget = SylphideProcessor.PageT()
 pageu = SylphideProcessor.PageU()
 
-page_list = ["A", "H", "M", "S", "U"]
-page_syl_list = [pagea, pageh, pagem, pages, pageu]
+page_list = ["A", "H", "L", "M", "N", "O", "P", "R", "S", "U"]
+page_syl_list = [pagea, pageh, pagel, pagem, pagen, pageo, pagep, pager, pages, pageu]
 page = dict(zip(page_list, page_syl_list))
-page_payloads_list = [9, 12, 3, 12, 14]
+page_payloads_list = [9, 12, 4, 3, 9, 4, 6, 6, 12, 14]
 page_payloads = dict(zip(page_list, page_payloads_list))
 page_dat = {key: [[] * i for i in range(page_payloads[key] + 1)] for key in page_list}
 page_updated = {key: False for key in page_list}
@@ -109,12 +114,52 @@ def update_plot_h():
         page_updated["H"] = False
 
 
+def update_plot_l():
+    global page_updated
+    if page_updated["L"]:
+        for i in range(page_payloads["L"]):
+            curve["L"][i].setData(page_dat["L"][0], page_dat["L"][i + 1])
+        page_updated["L"] = False
+
+
 def update_plot_m():
     global page_updated
     if page_updated["M"]:
         for i in range(page_payloads["M"]):
             curve["M"][i].setData(page_dat["M"][0], page_dat["M"][i + 1])
         page_updated["M"] = False
+
+
+def update_plot_n():
+    global page_updated
+    if page_updated["N"]:
+        for i in range(page_payloads["N"]):
+            curve["N"][i].setData(page_dat["N"][0], page_dat["N"][i + 1])
+        page_updated["N"] = False
+
+
+def update_plot_o():
+    global page_updated
+    if page_updated["O"]:
+        for i in range(page_payloads["O"]):
+            curve["O"][i].setData(page_dat["O"][0], page_dat["O"][i + 1])
+        page_updated["O"] = False
+
+
+def update_plot_p():
+    global page_updated
+    if page_updated["P"]:
+        for i in range(page_payloads["P"]):
+            curve["P"][i].setData(page_dat["P"][0], page_dat["P"][i + 1])
+        page_updated["P"] = False
+
+
+def update_plot_r():
+    global page_updated
+    if page_updated["R"]:
+        for i in range(page_payloads["R"]):
+            curve["R"][i].setData(page_dat["R"][0], page_dat["R"][i + 1])
+        page_updated["R"] = False
 
 
 def update_plot_s():
@@ -137,7 +182,12 @@ def update_plot_u():
 update_plot_list = [
     update_plot_a,
     update_plot_h,
+    update_plot_l,
     update_plot_m,
+    update_plot_n,
+    update_plot_o,
+    update_plot_p,
+    update_plot_r,
     update_plot_s,
     update_plot_u,
 ]
@@ -148,8 +198,9 @@ unprocess_list = []
 scaling = 1
 offset = 0
 
-scaling_u = [1,1,1,1,1,1,1,1,1,1,5e-3,10e-3,1e-3,1e-3]
-offset_u = [0,0,0,0,0,0,0,0,0,0,5.0,7.2,3.3,3.3]
+scaling_u = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5e-3, 10e-3, 1e-3, 1e-3]
+offset_u = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5.0, 7.2, 3.3, 3.3]
+
 
 def update_serial():
     global page_updated
@@ -161,19 +212,32 @@ def update_serial():
             dat = page[header].unpack(raw[4:36])
             page_updated[header] = True
             # 時刻の追加
-            if(header == 'M'):
+            if header == "M":
                 for j in range(4):
+                    page_dat[header][0].append(float(dat[j][1]) / 1000)
+            elif header == "R" or header =='O' or header == 'L':
+                for j in range(2):
                     page_dat[header][0].append(float(dat[j][1]) / 1000)
             else:
                 page_dat[header][0].append(float(dat[1]) / 1000)
             # データの追加
-            if header == 'U':
+            if header == "U":
                 for i in range(1, page_payloads[header] + 1):
-                    page_dat[header][i].append(float(dat[i + 1]) * scaling_u[i-1] + offset_u[i-1])
-            elif header == 'M':
+                    page_dat[header][i].append(
+                        float(dat[i + 1]) * scaling_u[i - 1] + offset_u[i - 1]
+                    )
+            elif header == "M":
                 for j in range(4):
                     for i in range(1, page_payloads[header] + 1):
-                        page_dat[header][i].append(float(dat[j][i + 1]) * scaling_u[i-1] + offset_u[i-1])
+                        page_dat[header][i].append(
+                            float(dat[j][i + 1]) * scaling + offset
+                        )
+            elif header == "R" or header =='O' or header == 'L':
+                for j in range(2):
+                    for i in range(1, page_payloads[header] + 1):
+                        page_dat[header][i].append(
+                            float(dat[j][i + 1]) * scaling + offset
+                        )
             else:
                 for i in range(1, page_payloads[header] + 1):
                     page_dat[header][i].append(float(dat[i + 1]) * scaling + offset)
@@ -259,6 +323,24 @@ curve_h = [
     p_ha.plot(pen=pg.mkPen("w", style=QtCore.Qt.DashLine), name="CH 8"),
 ]
 
+# Page L
+win_l = pg.GraphicsLayoutWidget(show=True, title="Basic plotting examples l")
+win_l.resize(1000, 600)
+win_l.setWindowTitle("Page L")
+
+p_l = win_l.addPlot(title="Strain")
+p_l.showGrid(x=True, y=True)
+p_l.setLabel("left", "Raw value", units="")
+p_l.setLabel("bottom", "Time", units="s")
+legend = p_l.addLegend(offset=(10, 10))
+
+curve_l = [
+    p_l.plot(pen="r", name="CH 1"),
+    p_l.plot(pen="g", name="CH 2"),
+    p_l.plot(pen="b", name="CH 3"),
+    p_l.plot(pen="w", name="CH 4"),
+]
+
 # Page M
 win_m = pg.GraphicsLayoutWidget(show=True, title="Basic plotting examples m")
 win_m.resize(1000, 600)
@@ -274,6 +356,111 @@ curve_m = [
     p_m.plot(pen="r", name="X"),
     p_m.plot(pen="g", name="Y"),
     p_m.plot(pen="b", name="Z"),
+]
+
+# Page N
+win_n = pg.GraphicsLayoutWidget(show=True, title="Basic plotting examples n")
+win_n.resize(1000, 600)
+win_n.setWindowTitle("Page N")
+
+p_n1 = win_n.addPlot(title="Navigation 1")
+p_n1.showGrid(x=True, y=True)
+p_n1.setLabel("left", "Raw value", units="")
+p_n1.setLabel("bottom", "Time", units="s")
+legend = p_n1.addLegend(offset=(10, 10))
+
+p_n2 = win_n.addPlot(title="Navigation 2")
+p_n2.showGrid(x=True, y=True)
+p_n2.setLabel("left", "Velocity", units="m/s")
+p_n2.setLabel("bottom", "Time", units="s")
+legend = p_n2.addLegend(offset=(10, 10))
+
+p_n3 = win_n.addPlot(title="Navigation 3")
+p_n3.showGrid(x=True, y=True)
+p_n3.setLabel("left", "Raw value", units="deg")
+p_n3.setLabel("bottom", "Time", units="s")
+legend = p_n3.addLegend(offset=(10, 10))
+
+curve_n = [
+    p_n1.plot(pen="r", name="Latitude"),
+    p_n1.plot(pen="g", name="Longitude"),
+    p_n1.plot(pen="b", name="Altitude"),
+    p_n2.plot(pen="r", name="N"),
+    p_n2.plot(pen="g", name="E"),
+    p_n2.plot(pen="b", name="D"),
+    p_n3.plot(pen="r", name="Yaw"),
+    p_n3.plot(pen="g", name="Roll"),
+    p_n3.plot(pen="b", name="Pitch"),
+]
+
+# Page O
+win_o = pg.GraphicsLayoutWidget(show=True, title="Basic plotting examples o")
+win_o.resize(1000, 600)
+win_o.setWindowTitle("Page O")
+
+p_o = win_o.addPlot(title="Strain")
+p_o.showGrid(x=True, y=True)
+p_o.setLabel("left", "Raw value", units="")
+p_o.setLabel("bottom", "Time", units="s")
+legend = p_o.addLegend(offset=(10, 10))
+
+curve_o = [
+    p_o.plot(pen="r", name="CH 1"),
+    p_o.plot(pen="g", name="CH 2"),
+    p_o.plot(pen="b", name="CH 3"),
+    p_o.plot(pen="w", name="CH 4"),
+]
+
+# Page P
+win_p = pg.GraphicsLayoutWidget(show=True, title="Basic plotting examples p")
+win_p.resize(1000, 600)
+win_p.setWindowTitle("Page P")
+
+p_p1 = win_p.addPlot(title="Pressure")
+p_p1.showGrid(x=True, y=True)
+p_p1.setLabel("left", "Raw value", units="")
+p_p1.setLabel("bottom", "Time", units="s")
+legend = p_p1.addLegend(offset=(10, 10))
+
+p_p2 = win_p.addPlot(title="Temperature")
+p_p2.showGrid(x=True, y=True)
+p_p2.setLabel("left", "Raw value", units="")
+p_p2.setLabel("bottom", "Time", units="s")
+legend = p_p2.addLegend(offset=(10, 10))
+
+curve_p = [
+    p_p1.plot(pen="r", name="CH 1"),
+    p_p1.plot(pen="g", name="CH 2"),
+    p_p1.plot(pen="b", name="CH 3"),
+    p_p2.plot(pen="r", name="CH 1"),
+    p_p2.plot(pen="g", name="CH 2"),
+    p_p2.plot(pen="b", name="CH 3"),
+]
+
+# Page R
+win_r = pg.GraphicsLayoutWidget(show=True, title="Basic plotting examples r")
+win_r.resize(1000, 600)
+win_r.setWindowTitle("Page R")
+
+p_r1 = win_r.addPlot(title="Pressure")
+p_r1.showGrid(x=True, y=True)
+p_r1.setLabel("left", "Raw value", units="")
+p_r1.setLabel("bottom", "Time", units="s")
+legend = p_r1.addLegend(offset=(10, 10))
+
+p_r2 = win_r.addPlot(title="Temperature")
+p_r2.showGrid(x=True, y=True)
+p_r2.setLabel("left", "Raw value", units="")
+p_r2.setLabel("bottom", "Time", units="s")
+legend = p_r2.addLegend(offset=(10, 10))
+
+curve_r = [
+    p_r1.plot(pen="r", name="CH 1"),
+    p_r1.plot(pen="g", name="CH 2"),
+    p_r1.plot(pen="b", name="CH 3"),
+    p_r2.plot(pen="r", name="CH 1"),
+    p_r2.plot(pen="g", name="CH 2"),
+    p_r2.plot(pen="b", name="CH 3"),
 ]
 
 # Page S
@@ -355,7 +542,7 @@ curve_u = [
 ]
 
 # curves
-curve_list = [curve_a, curve_h, curve_m, curve_s, curve_u]
+curve_list = [curve_a, curve_h, curve_l, curve_m, curve_n, curve_o, curve_p, curve_r, curve_s, curve_u]
 curve = dict(zip(page_list, curve_list))
 
 # reset buffer
